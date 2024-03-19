0ca4b61de67738469206c10f19ae7d61ef4dae1d
emmanauel olowu <emmanuelr20@yahoo.com>
2024-03-19 21:56:41 +0100

    final changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Outfit } from "next/font/google";
-import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 import Script from "next/script";
 import Head from "next/head";
-// import { useRef } from "react";
 
 const Font = Outfit({
   subsets: ["latin"],
@@ -21,15 +19,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const tawkMessengerRef = useRef();
   return (
     <html lang="en">
       <body className={Font.className}>{children}</body>
-      {/* <TawkMessengerReact
-        propertyId={"65f93f11a0c6737bd1225dc6"}
-        widgetId={"df9493c8dc9c2a6e9517d5453e0b726c5a12647b"}
-        // useRef={tawkMessengerRef}
-      /> */}
       <Script strategy="lazyOnload" src="https://embed.tawk.to/65f93f11a0c6737bd1225dc6/1hpapsnn3" />
       <Head>
         <link rel="icon" href="/circle-logo.png" />
